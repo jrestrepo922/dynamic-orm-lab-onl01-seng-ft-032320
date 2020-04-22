36793f62d0bfae38d0175617a8c776d2dd94bc5b
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-22 12:21:20 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -37,7 +37,14 @@ class InteractiveRecord
 
 # INSERT INTO TABLE songs(name, grade) VALUES("Tom", "10")
 
+  def values_for_insert 
+    values = [] 
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil? 
 
+    end 
+    values.join(", ")
+  end 
 
 
 
